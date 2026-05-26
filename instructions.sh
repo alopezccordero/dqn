3090cdd106337fd0c549662060277cc320393fd8
alopezcordero <alejandro.lopezcordero01@utrgv.edu>
2026-05-26 13:30:55 -0500

    dqn

---
+++ instructions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=spaceinvaders-dqn
+#SBATCH --output=spaceinvaders-dqn.out.%j
+#SBATCH --partition=gpua30q
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+#activate conda
+source ~/miniconda/etc/profile.d/conda.sh
+conda activate atari-project
+
+echo "Python"
+which python
+python --version
+
+echo "go into atari-project folder"
+cd ~/atari-project
+
+echo "Starting spaceinvaders training"
+
+python -m rl_zoo3.train --algo dqn --env SpaceInvadersNoFrameskip-v4 -f logs/ -c dqn.yml
+
+echo "training finished"
+
+echo "Evaluation"
+
+python -m rl_zoo3.enjoy --algo dqn --env SpaceInvadersNoFrameskip-v4 --no-render --n-timesteps 5000 --folder logs/
+
+echo "evaluation finished"
---
+++ testing-atari.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+import ale_py
+
+gym.register_envs(ale_py)
+
+env = gym.make("ALE/Pong-v5", render_mode="human")
+
+obs, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        obs, info = env.reset()
+
+env.close()
